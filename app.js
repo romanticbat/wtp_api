63e04ced9c6c00c5d2e2ecb4992b88e51cf238c5
Makoto <ninjmiley@gmail.com>
2025-04-28 14:37:17 -0300

    ping

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const https = require('https');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Rota para pegar a imagem
 app.get('/image/:id', (req, res) => {
   const id = req.params.id;
 
@@ -21,6 +23,23 @@ app.get('/image/:id', (req, res) => {
   res.sendFile(imagePath);
 });
 
+// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+
+  // Começa o sistema de auto-ping quando o servidor iniciar
+  const url = 'https://wtp-api.onrender.com'; // Sua URL
+  const interval = 5 * 60 * 1000; // 5 minutos
+
+  function ping() {
+    https.get(url, (res) => {
+      const now = new Date().toLocaleString('pt-BR');
+      console.log(`[${now}] Ping enviado! Status code: ${res.statusCode}`);
+    }).on('error', (err) => {
+      console.error('Erro ao enviar ping:', err.message);
+    });
+  }
+
+  ping(); // Faz o primeiro ping na hora
+  setInterval(ping, interval); // Depois pinga a cada 5 minutos
 });
